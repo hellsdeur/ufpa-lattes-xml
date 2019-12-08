e8b714975edd1adbf8e263cd0e477b250351c618
mtshelder <hmateus1997@gmail.com>
2019-12-08 00:30:03 -0300

    main files

---
+++ main.py
@@ -0,0 +1,25 @@
+def menu_principal():
+    pass
+
+def descompactar():
+    pass
+
+def extrair_nomes():
+    pass
+
+def juntar_professores():
+    pass
+
+def extrair_info():
+    pass
+
+def anexar_qualis_artigos():
+    pass
+
+def anexar_qualis_eventos():
+    pass
+
+def escrever_csv():
+    pass
+
+menu_principal()
